732140cbdedf8746875cb0df456bc119c17552c7
francisco-campayo <158186901+francisco-campayo@users.noreply.github.com>
2026-09-09 20:50:52 +0200

    feat: modularización y adición del ID al JSON

--- vision-service/src/occupancy_calculator.py
+++ vision-service/src/occupancy_calculator.py
@@ -1,1 +1,12 @@
 # Calculate density/occupation here
+
+def count_people(results):
+    """
+    Recibe los resultados de YOLO y devuelve el número total de personas (clase 0).
+    """
+    total = 0
+    for r in results:
+        for box in r.boxes:
+            if int(box.cls[0]) == 0:  # 0 es el ID de la clase 'persona' en COCO
+                total += 1
+    return total
--- vision-service/src/publisher.py
+++ vision-service/src/publisher.py
@@ -1,1 +1,24 @@
 # Here you manage how you send the data obtained from the video (API POST or MOM, better to use Kafka)
+
+import json
+import uuid
+from kafka import KafkaProducer
+
+class OccupancyPublisher:
+    def __init__(self, broker='kafka:9092', topic='occupancy_topic'):
+        self.topic = topic
+        # Inicializa la conexión a Kafka
+        self.producer = KafkaProducer(
+            bootstrap_servers=[broker],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
+    def publish(self, camera_id, total_people, timestamp):
+        # Genera el JSON con el ID único
+        mensaje = {
+            "id": str(uuid.uuid4()),
+            "timestamp": timestamp,
+            "camera_id": camera_id,
+            "total_people": total_people
+        }
+        self.producer.send(self.topic, value=mensaje)
