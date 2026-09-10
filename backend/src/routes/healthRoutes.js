2af67532186fddfc650219f80eec787416544a1e
yeraox <246516504+yeraox@users.noreply.github.com>
2026-09-10 12:31:28 +0200

    fix: problem with the routes name

--- backend/src/routes/healthRoutes.js
+++ backend/src/routes/healthRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getStatus } = require('../controllers/healthController');
 
-router.get('/health', getStatus);
+router.get('/', getStatus);
 
 module.exports = router;
