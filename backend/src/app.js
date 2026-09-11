ebe8665ce0972052ea744422c90406e17deec2d5
yeraox <246516504+yeraox@users.noreply.github.com>
2026-09-11 13:14:39 +0200

    feat(backend): done corsconfig and add security layers to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const healthRoutes = require('./routes/healthRoutes');
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet'; 
+import { express as useragent } from 'express-useragent';
 
+import healthRoutes from './routes/health.routes.js';
+
+import corsconfig from './config/cors.config.js';
+
+const API_VERSION = process.env.API_VERSION || 'v1';
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.set('trust proxy', true);
+app.use(express.json({limit: '10kb' , strict: true}));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(helmet());
+app.use(cors(corsconfig));
+app.use(useragent());
 
-app.use('/health', healthRoutes);
+app.use(`/${API_VERSION}/health`, healthRoutes);
+app.use(`/${API_VERSION}/metrics`, metricsRoutes);
 
-module.exports = app;
+export default app;
