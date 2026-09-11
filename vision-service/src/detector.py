dee7bdd54ba4458f286355743063098a77fa9d96
francisco-campayo <158186901+francisco-campayo@users.noreply.github.com>
2026-09-11 15:35:52 +0200

    fix: actualizar envio de mensaje de datos leidos por la cámara

--- vision-service/src/detector.py
+++ vision-service/src/detector.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from datetime import datetime, timezone
 from ultralytics import YOLO
 
@@ -14,27 +15,42 @@ def main():
     # 2. Cargar fuente de datos (la ruta relativa dentro de Docker) Ruta absoluta dentro del Docker
     cap = cv2.VideoCapture("/app/Dataset/mall_dataset/frames/seq_%06d.jpg")
 
+    # Variable para comprobar si ha habido un cambio en la cantidad de personas en la imágen
+    last_amount_people = None
+    publish_period = 2.0  # <-- 2. Aquí puedes cambiar el tiempo (ej: 0.5, 2.0 segundos)
+    last_publish_time = 0.0 # Guarda la hora exacta del último envío
+
     # 3. Bucle principal de inferencia
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break  # Fin de la secuencia de imágenes
         
-        # A. Detectar con YOLO
+        # Detectar con YOLO
         results = model(frame, verbose=False)
         
-        # B. Calcular métricas usando el módulo externo
+        # Calcular métricas usando el módulo externo
         personas_detectadas = count_people(results)
-        
-        # C. Publicar a Kafka usando el módulo externo
-        tiempo_actual = datetime.now(timezone.utc).isoformat()
-        publisher.publish(
-            camera_id="zona_centro_comercial",
-            total_people=personas_detectadas,
-            timestamp=tiempo_actual
-        )
-        
-        print(f"Enviado: {personas_detectadas} personas detectadas.")
+
+        # Comprobación de condiciones para publicar
+        current_time = time.time() # Obtenemos el reloj actual en segundos
+
+        # Solo entraremos si ocurren AMBAS cosas
+        if (personas_detectadas != last_amount_people) and ((current_time - last_publish_time) >= publish_period):
+            tiempo_actual_iso = datetime.now(timezone.utc).isoformat()
+
+            publisher.publish(
+                camera_id="zona_centro_comercial",
+                total_people=personas_detectadas,
+                timestamp=tiempo_actual_iso
+            )
+            
+            print(f" Enviado: {personas_detectadas} personas detectadas.")
+
+            # Actualizamos las variables DENTRO del if
+            # Así solo registramos el estado cuando realmente sale un mensaje a Kafka
+            last_amount_people = personas_detectadas
+            last_publish_time = current_time
 
     cap.release()
 
