59aee96ea6f44d55f16c8bcf2ac04a3715e95f9f
yeraox <246516504+yeraox@users.noreply.github.com>
2026-09-11 13:11:24 +0200

    refactor(services): change imports and exports also add a filter for total_people

--- backend/src/services/metricsService.js
+++ backend/src/services/metricsService.js
@@ -1,14 +1,19 @@
 //Here we apply validations about the metrics we are received from the kafka consumer
 const metricsModel = require('../models/metricsModel');
 
-const processDetectedMetrics = async (metrics) => {
+/**
+ * Processes detected metrics and saves them to the database or discards them based on certain conditions.
+ * @param {Object} metrics - The metrics object received from the kafka consumer
+ * @param {number} lastTotalPeople - The last recorded total people count
+ * @returns {Promise<void>}
+ */
+const processDetectedMetrics = async (metrics, lastTotalPeople) => {
     const { id, timestamp, camera_id, total_people } = metrics;
 
     if (total_people === undefined || total_people < 0) return; // Ignore invalid metrics
-
-   return await metricsModel.saveMetrics(id, timestamp, camera_id, total_people);
+    if (total_people === lastTotalPeople) return; // Ignore if the total_people count hasn't changed
+   
+    return await metricsModel.saveMetrics(id, timestamp, camera_id, total_people);
 };
 
-module.exports = {
-    processDetectedMetrics
-};
+export default processDetectedMetrics;
